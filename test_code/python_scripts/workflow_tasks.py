0d457a4f0511a5524092af2e14685cf85aef4ff0
Arthur Dayton <arthur@observeinc.com>
2022-12-18 16:16:07 -0800

    clouds

--- test_code/python_scripts/workflow_tasks.py
+++ test_code/python_scripts/workflow_tasks.py
@@ -3,7 +3,7 @@
 import os
 
 
-def tf_override_file(test_group="", override_file_path="../override.tf"):
+def tf_override_file(cloud="", test_group="", override_file_path="../override.tf"):
 
     with open(override_file_path, "w+") as myfile:
         myfile.write(
@@ -13,7 +13,7 @@ def tf_override_file(test_group="", override_file_path="../override.tf"):
             backend "s3" {{
                 bucket = "thunderdome-terraform-state"
                 region = "us-west-2"
-                key    = "content-eng/gha/aws/linuxhost_{test_group}"
+                key    = "content-eng/gha/{cloud}/linuxhost_{test_group}"
             }}
             }}
 
@@ -54,6 +54,53 @@ def tf_main_file(module="", main_file_path="../main.tf"):
                     aws = aws
                     }}
                 }}
+                """
+            )
+        elif module == "gcp_machines":
+            myfile.write(
+                f"""
+                locals {{
+                    name_format = var.CI == true ? "gha-lht-${{var.WORKFLOW_MATRIX_VALUE}}-%s" : "linux-host-test-%s"
+                }}
+
+                module "gcp_machines" {{
+                    source             = "./GCP_MACHINES"
+                    public_key_path    = var.PUBLIC_KEY_PATH
+                    PRIVATE_KEY_PATH   = var.PRIVATE_KEY_PATH
+                    region             = "us-west1"
+                    zone               = "a"
+                    name_format        = local.name_format
+                    GCP_COMPUTE_FILTER = []
+                    CI                 = var.CI
+                    PUBLIC_KEY         = var.PUBLIC_KEY
+
+                    providers = {{
+                        google = google
+                    }}
+                }}
+                """
+            )
+
+        elif module == "azure_machines":
+            myfile.write(
+                f"""
+                locals {{
+                    name_format = var.CI == true ? "gha-lht-${{var.WORKFLOW_MATRIX_VALUE}}-%s" : "linux-host-test-%s"
+                }}
+
+                module "azure_machines" {{
+                    source               = "./AZURE_MACHINES"
+                    public_key_path      = var.PUBLIC_KEY_PATH
+                    PRIVATE_KEY_PATH     = var.PRIVATE_KEY_PATH
+                    location             = "West US 3"
+                    name_format          = local.name_format
+                    AZURE_COMPUTE_FILTER = ["UBUNTU_18_04_LTS", "UBUNTU_20_04_LTS", "UBUNTU_22_04_LTS", "RHEL_8", "CENTOS_8"]
+                    CI                   = var.CI
+                    PUBLIC_KEY           = var.PUBLIC_KEY
+                    providers = {{
+                        azurerm = azurerm
+                    }}
+                    }}
                 """
             )
 
