824edd0c3c72452eb75b0b58edca3e98ab5502a9
Arthur Dayton <arthur@observeinc.com>
2023-01-27 13:43:38 -0800

    amzon linux fix

--- observe_configure_script.sh
+++ observe_configure_script.sh
@@ -643,8 +643,8 @@ EOT
 
 cat <<EOF | sudo tee /etc/yum.repos.d/influxdb.repo
 [influxdb]
-name = InfluxDB Repository - RHEL \$releasever
-baseurl = https://repos.influxdata.com/rhel/\$releasever/\$basearch/stable
+name = InfluxDB Repository - RHEL 7
+baseurl = https://repos.influxdata.com/rhel/7/\$basearch/stable
 enabled = 1
 gpgcheck = 1
 gpgkey = https://repos.influxdata.com/influxdata-archive_compat.key
