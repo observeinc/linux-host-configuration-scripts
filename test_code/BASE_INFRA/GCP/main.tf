6eea4e6613ff6fad30bf1f7b1e055701efc2813c
Arthur Dayton <arthur@observeinc.com>
2023-03-16 09:05:55 -0700

    add sewrvice account

---
+++ test_code/BASE_INFRA/GCP/main.tf
@@ -0,0 +1,26 @@
+locals {
+  str_f = "_"
+  str_r = "-"
+}
+
+resource "google_service_account" "compute" {
+  account_id   = format(lower(replace(var.name_format, local.str_f, local.str_r)), "sa")
+  display_name = "Service Account for compute resources"
+  project      = var.project_id
+}
+
+resource "google_project_iam_member" "compute" {
+  depends_on = ["null_resource.delay"]
+  for_each = toset([
+    "roles/compute.admin",
+    "roles/osconfig.osPolicyAssignmentAdmin",
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter",
+    "roles/storage.objectAdmin",
+    "roles/bigquery.admin"
+  ])
+
+  project = var.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.compute.email}"
+}
