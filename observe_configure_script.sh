d78e66df0d2345a0ff430031d4689f9a9270e111
Dani Klein <dhklein92@gmail.com>
2023-03-27 18:16:27 -0500

    feat: add osquery installation support for AL2023 (#39)

--- observe_configure_script.sh
+++ observe_configure_script.sh
@@ -620,6 +620,8 @@ case ${OS} in
 
     log "Amazon OS"
 
+    export AL_VERSION=$(awk -F= '$1=="VERSION" { print $2 ;}' /etc/os-release)
+
       #####################################
       # osquery
       #####################################
@@ -629,9 +631,16 @@ case ${OS} in
 
         curl -L https://pkg.osquery.io/rpm/GPG | sudo tee /etc/pki/rpm-gpg/RPM-GPG-KEY-osquery
 
-        sudo yum-config-manager --add-repo https://pkg.osquery.io/rpm/osquery-s3-rpm.repo
-        sudo yum-config-manager --enable osquery-s3-rpm-repo
-        sudo yum install osquery -y
+        if [ "$AL_VERSION" == "2023" ]; then
+          sudo dnf config-manager --add-repo https://pkg.osquery.io/rpm/osquery-s3-rpm.repo
+          sudo dnf config-manager --enable osquery-s3-rpm-repo
+          sudo dnf install osquery -y
+        else
+          sudo yum-config-manager --add-repo https://pkg.osquery.io/rpm/osquery-s3-rpm.repo
+          sudo yum-config-manager --enable osquery-s3-rpm-repo
+          sudo yum install osquery -y
+        fi
+
         sudo service osqueryd start 2>/dev/null || true
         sudo systemctl enable osqueryd
 
