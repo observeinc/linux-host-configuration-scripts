6e18c8cd8a5e2b2bec094809ebd7fcc9480428ea
Yasar Hussain <yasar@observeinc.com>
2023-09-22 15:25:20 -0700

    add windows machines to outputs

--- test_code/AZURE_MACHINES/outputs.tf
+++ test_code/AZURE_MACHINES/outputs.tf
@@ -1,5 +1,6 @@
 output "fab_hosts" {
-  value = { for key, value in azurerm_linux_virtual_machine.linux_host_test :
+  value = merge({ 
+    for key, value in azurerm_linux_virtual_machine.linux_host_test :
     "AZURE_${key}" => {
       "host" = value.public_ip_address
       "name" = value.name
@@ -10,5 +11,19 @@ output "fab_hosts" {
       "public_ssh_link" = "ssh -i ${var.PRIVATE_KEY_PATH} ${var.AZURE_MACHINE_CONFIGS[key].default_user}@${value.public_ip_address}"
       "sleep" : var.AZURE_MACHINE_CONFIGS[key].sleep
     }
+  },
+  {
+    for key, value in azurerm_windows_virtual_machine.windows_host_test :
+    "AZURE_${key}" => {
+      "host" = value.public_ip_address
+      "name" = value.name
+      "user" = var.AZURE_WIN_MACHINE_CONFIGS[key].default_user
+      "connect_kwargs" = {
+        "key_filename" : var.PRIVATE_KEY_PATH
+      }
+      "public_ssh_link" = "ssh -i ${var.PRIVATE_KEY_PATH} ${var.AZURE_MACHINE_CONFIGS[key].default_user}@${value.public_ip_address}"
+      "sleep" : var.AZURE_WIN_MACHINE_CONFIGS[key].sleep
+    }
   }
+  )
 }
